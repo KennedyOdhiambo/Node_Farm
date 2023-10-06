6897fabd8dee39a0dc6431461e87335f75eab546
kennedy <x.johnkennedy.x@gmail.com>
2023-10-06 08:22:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//sysnchronour
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+//asynchronous
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//   });
+// });
+// console.log("Will read file!");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to requests from port 8000");
+});
